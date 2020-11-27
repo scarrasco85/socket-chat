d9da72942aff0eaeea3a7482ff63dee7e6f821ab
scarrasco85 <chechucarrasco@gmail.com>
2020-11-27 13:25:48 +0100

    User class created

---
+++ server/classes/users.js
@@ -0,0 +1,54 @@
+class Users {
+
+    constructor() {
+
+        this.users = [];
+
+    }
+
+    // Add a person to chat
+    addUser(id, userName) {
+
+        let user = { id, userName };
+
+        this.users.push(user);
+
+        return this.users;
+    }
+
+    // Get user by ID
+    getUser(id) {
+
+        // Regresa un array con todos los elementos que coincidan en la condición. Cogemos el primer elemento
+        // que coincida aunque sabemos que el id será único
+        let user = this.users.filter(person => person.id === id)[0];
+
+        // Si no encuentra una persona user tendrá undefined
+        return user;
+    }
+
+    getUsers() {
+
+        return this.users;
+    }
+
+    getUsersByChatRoom(id) {
+
+        let userDeleted = this.getUser(id);
+
+        // Regresa un array con todos los usuarios menos el que coincide con el id
+        this.users = this.users.filter(user => user.id != id);
+
+        // //Esto es lo mismo que lo anterior
+        // // Regresa un array con todos los usuarios menos el que coincide con el id
+        // this.users = this.users.filter(user => {
+        //     return user.id != id;
+        // });
+
+        return userDeleted;
+    }
+}
+
+module.exports = {
+    Users
+}
