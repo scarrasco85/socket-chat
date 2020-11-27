f05d4b60a6c332d88b7aec64be25db6fc1024a27
scarrasco85 <chechucarrasco@gmail.com>
2020-11-27 17:10:32 +0100

    Conect User to chat - entryToChat

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -3,8 +3,27 @@
 // var to have higth compatibility
 var socket = io();
 
+var params = new URLSearchParams(window.location.search);
+
+if (!params.has('name')) {
+
+    window.location = 'index.html';
+    throw new Error('Name is required');
+}
+
+var user = {
+    name: params.get('name')
+};
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
+
+    // Cuando un usuario se conecta al chat se recibe una callback con una respuesta donde viene un array con
+    // todos los usuarios conectados
+    // Solicita que un usuario entre al chat
+    socket.emit('entryToChat', user, function(resp) {
+        console.log('users conected: ', resp);
+    });
 });
 
 socket.on('disconnect', function() {
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,45 +1,33 @@
+const { Socket } = require('socket.io');
 const { io } = require('../server');
+const { Users } = require('../classes/users');
+
+const user = new Users();
 
 // Clients conections
 io.on('connection', (client) => {
 
     console.log('Usuario conectado');
 
-    client.emit('enviarMensaje', {
-        user: 'Admin',
-        message: 'Welcome to this app'
+    // Escucha cuando un usuario entra al chat, añade al usuario al array de Usuarios conectados y le devuelve
+    // el array al usuario que se acaba de conectar
+    client.on('entryToChat', (data, callback) => {
+
+        if (!data.name) {
+            return callback({
+                error: true,
+                message: 'Name is required'
+            });
+        }
+
+        // El id del usuario lo obtenemos del id del objeto client - devuelve array con todos los usuarios
+        let usersConected = user.addUser(client.id, data.name);
+        console.log(user, 'ha entrado en el chat');
+
+        // Cuando un usuario se conecta al chat se le devuelve un array con todos los usuarios conectados incluido
+        // él mismo
+        callback(usersConected);
+
     });
-
-
-    client.on('disconnect', () => {
-        console.log('Usuario desconectado');
-    });
-
-    // Listening 'enviarMensaje' of clients
-    client.on('enviarMensaje', (data) => {
-
-        console.log(data);
-
-        // Broadcast 'enviarMensaje'
-        client.broadcast.emit('enviarMensaje', data);
-
-    });
-
-    // Listening 'enviarMensaje' of clients
-    //  client.on('enviarMensaje', (data, callback) => {
-
-    //     console.log(data);
-
-    //     if (message.user) {
-    //         callback({
-    //             resp: 'Todo salió bien'
-    //         });
-    //     } else {
-    //         callback({
-    //             resp: 'Algo salió mal'
-    //         });
-    //     }
-
-    // });
 
 });
